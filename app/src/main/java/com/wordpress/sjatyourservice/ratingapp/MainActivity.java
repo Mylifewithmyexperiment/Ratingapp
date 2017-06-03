1ef60b4b53d294e722fca6ac3c4558ed1d021ebe
SHASHI <shashi.jaiswal17@gmail.com>
2017-06-04 01:53:52 +0530

    testing first project

---
+++ app/src/main/java/com/wordpress/sjatyourservice/ratingapp/MainActivity.java
@@ -0,0 +1,165 @@
+package com.wordpress.sjatyourservice.ratingapp;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+    private TextView mlaunchCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);//no need to define toolbar
+        setSupportActionBar(toolbar);
+
+
+            //from here progm for rate us dialog starts!!!
+
+
+//for counting each launch of app
+        mlaunchCount = (TextView) findViewById(R.id.launchCount);
+        AppPreferences.getInstance(getApplicationContext()).incrementLaunchCount();
+        mlaunchCount.setText(getString(R.string.app_message,
+                AppPreferences.getInstance(getApplicationContext()).getLaunchCount()));
+        showRateAppDialogIfNeeded();
+    }
+
+// setting number of  specified launch(here 3) to shows dialog box of rating
+    private void showRateAppDialogIfNeeded() {
+        boolean bool = AppPreferences.getInstance(getApplicationContext()).getAppRate();
+        int i = AppPreferences.getInstance(getApplicationContext()).getLaunchCount();
+        if ((bool) && (i == 3)) {
+            createAppRatingDialog(getString(R.string.rate_app_title), getString(R.string.rate_app_message)).show();
+        }
+    }
+
+//after specified dialog showing rate option dialog
+
+    private AlertDialog createAppRatingDialog(String rateAppTitle, String rateAppMessage) {
+        AlertDialog dialog = new AlertDialog.Builder(this).setPositiveButton(getString(R.string.dialog_app_rate), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface paramAnonymousDialogInterface, int paramAnonymousInt) {
+                openAppInPlayStore(MainActivity.this);
+                AppPreferences.getInstance(MainActivity.this.getApplicationContext()).setAppRate(false);
+            }
+        }).setNegativeButton(getString(R.string.dialog_your_feedback), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface paramAnonymousDialogInterface, int paramAnonymousInt) {
+                openFeedback(MainActivity.this);
+                AppPreferences.getInstance(MainActivity.this.getApplicationContext()).setAppRate(false);
+            }
+        }).setNeutralButton(getString(R.string.dialog_ask_later), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface paramAnonymousDialogInterface, int paramAnonymousInt) {
+                paramAnonymousDialogInterface.dismiss();
+                AppPreferences.getInstance(MainActivity.this.getApplicationContext()).resetLaunchCount();
+            }
+        }).setMessage(rateAppMessage).setTitle(rateAppTitle).create();
+        return dialog;
+    }
+
+
+    //for opening app in play store
+    public static void openAppInPlayStore(Context paramContext) {
+        paramContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/")));
+    }
+//for feedback directly to mail id
+    public static void openFeedback(Context paramContext) {
+        Intent localIntent = new Intent(Intent.ACTION_SEND);
+        localIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"shashi.jaiswal17@gmail.com"});
+        localIntent.putExtra(Intent.EXTRA_CC, "");
+        String str = null;
+        try {
+            str = paramContext.getPackageManager().getPackageInfo(paramContext.getPackageName(), 0).versionName;
+            localIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback for Your Android App");
+            localIntent.putExtra(Intent.EXTRA_TEXT, "\n\n----------------------------------\n Device OS: Android \n Device OS version: " +
+                    Build.VERSION.RELEASE + "\n App Version: " + str + "\n Device Brand: " + Build.BRAND +
+                    "\n Device Model: " + Build.MODEL + "\n Device Manufacturer: " + Build.MANUFACTURER);
+            localIntent.setType("message/rfc822");
+            paramContext.startActivity(Intent.createChooser(localIntent, "Choose an Email client :"));
+        } catch (Exception e) {
+            Log.d("OpenFeedback", e.getMessage());
+        }
+    }
+
+
+
+
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+/**
+ *
+ *
+
+
+
+
+
+
+ @Override
+ public boolean onCreateOptionsMenu(Menu menu) {
+ // Inflate the menu; this adds items to the action bar if it is present.
+ getMenuInflater().inflate(R.menu.menu_main, menu);
+ return true;
+ }
+
+ @Override
+ public boolean onOptionsItemSelected(MenuItem item) {
+ // Handle action bar item clicks here. The action bar will
+ // automatically handle clicks on the Home/Up button, so long
+ // as you specify a parent activity in AndroidManifest.xml.
+ int id = item.getItemId();
+
+ //noinspection SimplifiableIfStatement
+ if (id == R.id.action_settings) {
+ return true;
+ }
+
+ return super.onOptionsItemSelected(item);
+ }
+
+ }
+
+ *
+ **/
